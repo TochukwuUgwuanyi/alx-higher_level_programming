1aa7c98a2eae067915e64714251f1d341dab7ae5
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-03 15:51:27 -0800

    python slicing

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[7:]
 middle_word = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
+print(f"First 3 letters: {word_first_3}\n")
+print(f"Last 2 letters: {word_last_2}\n")
 print(f"Middle word: {middle_word}")
