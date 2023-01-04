4109bc8b56e11b1731a96a742bf10d67ba25c1bd
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-04 14:57:36 -0800

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-import string
-for i in (string.ascii_lowercase[:4]+string.ascii_lowercase[5:16]+string.ascii_lowercase[17:]):
-    print("{:s}".format(i), end='')
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
