5118c3c467854355101407f3d84545011197161b
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-05 03:09:07 -0800

    a program that prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 a = number % 10
 if (a > 5):
     print(f"Last digit of {number} is {a} and is greater than 5")
-elif (a == 0):
+elif(a == 0):
     print(f"Last digit of {number} is {a} and is 0")
 else:
     print(f"Last digit of {number} is {a} and is less than 6 and not 0\n")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(0, 99):
+    a = hex(i)
+    print(i, "=", a)
