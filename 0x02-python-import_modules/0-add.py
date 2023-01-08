a77807a3f4b3a3e04943c52230b302178941cc77
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-08 02:41:14 -0800

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-add = __import__('add_0').add
+if __name__ == "__main__":
+    from add_0 import add
+    
+    a = 1
+    b = 2
 
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+print("{} + {} = {}".format(a, b, add(a, b)))
