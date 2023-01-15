71f154ec4955c2cd60b3f30e1c8c32c0419fa609
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-15 12:48:04 -0800

    a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """Prints all names defined by compiled module."""
+
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
