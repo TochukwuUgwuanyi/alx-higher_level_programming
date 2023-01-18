043005fa97b57c48aca921cb50fd7ec07a3f82d2
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-18 13:26:36 -0800

    Python function -Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-import dis
-
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if (a < b):
@@ -12,6 +9,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return (sub(a, b))
-
-
-dis.dis(magic_calculation)
