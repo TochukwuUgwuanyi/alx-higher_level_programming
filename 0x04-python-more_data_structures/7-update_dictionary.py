dc32a1e6dd050a3dfe60eaec00d77dac9cbfda37
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-01-23 14:34:02 -0800

    a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,7 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return (a_dictionary)
 
+
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.key()):
-        print ("{} : {}".format(key, a_dictionary[key]))
+    for key in sorted(a_dictionary.keys()):
+        print("{} : {}".format(key, a_dictionary[key]))
