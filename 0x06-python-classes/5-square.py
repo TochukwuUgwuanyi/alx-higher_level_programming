2559b928951bbd90ca5624a7b7063bc3db2c0d66
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-02-03 06:37:23 -0800

    a class that Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define a Class Square"""
+
+
+class Square:
+    """Defining a class square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of a square"""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """print the square with # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if (self.__size == 0):
+            print("")
