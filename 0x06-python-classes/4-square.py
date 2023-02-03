ff91d288228bd8b8502424f77b3bafcb2bba05a5
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-02-03 06:05:21 -0800

    a class that Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define a Class Square"""
+
+
+class Square:
+    """Defining a class square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Return the area of a square"""
+        return(self.__size * self.__size)
