1c1991a326f4e16e1dc849e5797092fd8be34f6d
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-02-06 11:28:41 -0800

    a class program to calculate Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Define a Class Rectangle"""
+
+
+class Rectangle:
+    """Defining a class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Get/set the current width of the rectangle."""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the current height of the rectangle."""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of a square"""
+        return(self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the area of a square"""
+        return(2 * (self.__width + self.__height))
+        if (width == 0 or height == 0):
+            return(0)
