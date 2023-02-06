e4cb055871cd29fa231899363fd9e0aef05429cb
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-02-06 11:15:12 -0800

    updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Defining a class Rectangle"""
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -17,7 +17,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif (value < 0):
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,6 +30,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integers")
-        elif (value < 0):
+        if (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
