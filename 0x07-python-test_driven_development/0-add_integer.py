b7c4a20c6165ad69cca919a2c84ce3556fea285c
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-02-11 05:58:52 -0800

    a function that adds 2 integers with exception message

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ Return the integer addition of a and b. and raises expection"""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))
