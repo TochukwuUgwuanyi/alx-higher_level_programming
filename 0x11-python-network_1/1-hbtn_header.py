85c8cd22ddbe2ec1693f61af4df6e29d90db57f4
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-04-30 07:08:44 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL
 and displays the value of the `X-Request-Id`
-variable found in the header of the response.
+variable found in the header of the response
 """
 
 from sys import argv
